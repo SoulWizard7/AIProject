9eb86124ff6ebde4ccd433389c6bf95ee2048e7e
Niklas Högnabba <n.hognabba@gmail.com>
2023-03-06 12:35:10 +0100

    Init commit

---
+++ Source/AITesting/AITestingGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AITestingGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAITestingGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAITestingGameMode();
+};
+
+
+
